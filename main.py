27346fb9c9680c59da80266bfaea97a80936dfce
Jeff07K <jabejarano09@ucatolica.edu.co>
2026-03-03 15:44:51 -0500

    esta horrible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from paydantic import BaseModel
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -10,12 +10,18 @@ class Pokemon(BaseModel):
     attack: int
     vivo: bool
 
-new_pokemon_db = [Pokemon(id=1, name="Gengar", hp=60, attack=65, vivo=True),
-                   Pokemon(id=2, name="Charizard", hp=78, attack=84, vivo=True),
-                   Pokemon(id=3, name="Pikachu", hp=35, attack=55, vivo=True),
-                   Pokemon(id=4, name="Mewtwo", hp=106, attack=110, vivo=True),
-                   Pokemon(id=5, name="Bulbasaur", hp=45, attack=49, vivo=True),
-                   Pokemon(id=6, name="Squirtle", hp=44, attack=48, vivo=True)]
+p1=Pokemon(id=1, name="Gengar", hp=60, attack=65, vivo=True),
+p2=Pokemon(id=2, name="Charizard", hp=78, attack=84, vivo=True),               
+p3=Pokemon(id=3, name="Pikachu", hp=35, attack=55, vivo=True),
+p4=Pokemon(id=4, name="Mewtwo", hp=106, attack=110, vivo=True),
+p5=Pokemon(id=5, name="Bulbasaur", hp=45, attack=49, vivo=True),
+p6=Pokemon(id=6, name="Squirtle", hp=44, attack=48, vivo=True),
+p7=Pokemon(id=7, name="Raichu", hp=60, attack=90, vivo=True),
+p8=Pokemon(id=8, name="Eevee", hp=55, attack=55, vivo=True),
+p9=Pokemon(id=9, name="Snorlax", hp=160, attack=110, vivo=True),
+p10=Pokemon(id=10, name="Togepi", hp=35, attack=20, vivo=True)
+
+new_pokemon:list[Pokemon] = [p1,p2,p3,p4,p5,p6,p7,p8,p9,p10]
 
 pokemon_db = [{"name":"Gengar"},
               {"name":"charizar"},
@@ -28,7 +34,15 @@ pokemon_db = [{"name":"Gengar"},
               {"name":"Snorlax"},
               {"name":"Togepi"}]
 
-@app.get ("/pokemoncho/")
+@app.get("/allpokemon/")
+def show_all_pokemon():
+    return new_pokemon
+
+@app.get("/onepokemon/")
+def show_one_pokemon(pos:int=0):
+    return new_pokemon[pos]
+
+@app.get ("/pokemon/")
 def show_pokemon(skip: int = 0, limit: int = 3):
     return pokemon_db[skip:skip+limit]
 
